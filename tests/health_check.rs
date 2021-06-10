58e9e1ad7d6e5c5f374f1707ea277eb4a756bb13
Falko <falko.dulat@gmail.com>
2021-06-09 22:19:25 -0700

    Implemened NOP /subscribe endpoint wit form validation

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -22,3 +22,50 @@ fn spawn_app() -> String {
     tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
+
+#[actix_rt::test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    let server_address = spawn_app();
+    let client = Client::new();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    let response = client
+        .post(&format!("{}/subscriptions", server_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[actix_rt::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let server_address = spawn_app();
+    let client = Client::new();
+
+    let test_cases = vec![
+        ("name=foo", "missing the email"),
+        ("email=foo%40bar.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &server_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The api did not fail with 400 when the payload was {}.",
+            error_message
+        );
+    }
+}
